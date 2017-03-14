bbe45201d4fd6b3e98f2331e44a6848865939186
Lazuli26 <alberthsalascalero@gmail.com>
2017-03-13 19:23:05 -0600

    Crear e imprimir respuesta

---
+++ Analisis.Proyecto1/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analisis.Proyecto1
+{
+    class Item
+    {
+        public int arriba, abajo, izquierda, derecha;
+        public Item(int ar,int ab,int iz,int der)
+        {
+            arriba = ar;
+            abajo = ab;
+            izquierda = iz;
+            derecha = der;
+        }
+        public Item()
+        {
+
+        }
+    }
+}
